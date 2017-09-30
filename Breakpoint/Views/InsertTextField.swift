cfacee0fad6f3023602a484c103543acefcc6f7b
Borombo <e.rombo@yahoo.com>
2017-09-30 13:27:43 +0200

    Auth & login UI

---
+++ Breakpoint/Views/InsertTextField.swift
@@ -0,0 +1,33 @@
+//
+//  InsertTextField.swift
+//  Breakpoint
+//
+//  Created by Erwan Rombo on 30/09/2017.
+//  Copyright © 2017 Borombo. All rights reserved.
+//
+
+import UIKit
+
+class InsertTextField: UITextField {
+
+    private var padding = UIEdgeInsetsMake(0, 20, 0, 0)
+    
+    override func awakeFromNib() {
+        let placeholder = NSAttributedString(string: self.placeholder!, attributes: [NSAttributedStringKey.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+        self.attributedPlaceholder = placeholder
+        super.awakeFromNib()
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+    override func placeholderRect(forBounds bounds: CGRect) -> CGRect {
+        return UIEdgeInsetsInsetRect(bounds, padding)
+    }
+    
+}
